b0bfbcb4b6e629ec1778dc44282861f0fb81197b
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 19:16:03 +0200

    feat: se añade clase vista para el menú de cliente

---
+++ app/SoroFest/src/main/java/view/ClienteView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class ClienteView {
+
+    public void mostrarMenuClientes(){
+        System.out.println("--- GESTIÓN DE CLIENTES ---");
+        System.out.println("1. Dar de alta cliente");
+        System.out.println("2. Listar clientes");
+        System.out.println("3. Buscar cliente");
+        System.out.println("4. Modificar cliente");
+        System.out.println("5. Eliminar cliente");
+        System.out.println("0. Volver");
+        System.out.print("Selecciona una opción: ");
+    }
+}
