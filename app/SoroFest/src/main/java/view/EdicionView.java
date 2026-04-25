ed12274986a986a9f5e6a4cbba24c9292d16923d
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 23:29:48 +0200

    feat: se crea vista de la clase edición para consutar sus datos y modificarlos

---
+++ app/SoroFest/src/main/java/view/EdicionView.java
@@ -0,0 +1,12 @@
+package view;
+
+public class EdicionView {
+
+    public void mostrarMenuEdicion(){
+        System.out.println("--- GESTIÓN DE LA EDICIÓN SOROFEST CADIZ ---");
+        System.out.println("1. Ver datos de la edición");
+        System.out.println("2. Modificar edición");
+        System.out.println("0. Volver");
+        System.out.println("Selecciona una opción");
+    }
+}
