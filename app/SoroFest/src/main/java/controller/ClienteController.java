2e5fcf84897760ce1cf4b1eeed7471255eea8e9f
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 22:14:47 +0200

    feat: se añaden funcionalidades dar de alta y listar clientes

--- app/SoroFest/src/main/java/controller/ClienteController.java
+++ app/SoroFest/src/main/java/controller/ClienteController.java
@@ -1,16 +1,22 @@
 package controller;
 
+import model.Cliente;
 import view.ClienteView;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClienteController {
     private Scanner scanner;
     private ClienteView clienteView;
+    private List<Cliente> listaClientes;
 
     public ClienteController(){
         this.scanner = new Scanner(System.in);
         this.clienteView = new ClienteView();
+        this.listaClientes = new ArrayList<>();
     }
 
     public void iniciarMenuCliente(){
@@ -22,8 +28,8 @@ public class ClienteController {
                 scanner.nextLine();
 
                 switch (opcion) {
-                    case 1 -> System.out.println("Alta de cliente");
-                    case 2 -> System.out.println("Listar clientes");
+                    case 1 -> darAltaCliente();
+                    case 2 -> listarClientes();
                     case 3 -> System.out.println("Buscar cliente");
                     case 4 -> System.out.println("Modificar cliente");
                     case 5 -> System.out.println("Eliminar cliente");
@@ -36,5 +42,38 @@ public class ClienteController {
                 opcion = -1;
             }
         } while (opcion != 0);
+    }
+
+    public void darAltaCliente(){
+        System.out.println("Introduce DNI: ");
+        String dni = scanner.nextLine();
+        System.out.println("Introduce nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Introduce apellidos: ");
+        String apellidos = scanner.nextLine();
+        System.out.println("Introduce email: ");
+        String email = scanner.nextLine();
+        System.out.println("Introduce teléfono: ");
+        String telefono = scanner.nextLine();
+        System.out.println("Introduce fecha de nacimiento (yyyy-MM-dd)");
+        LocalDate fechaNacimiento = LocalDate.parse(scanner.nextLine());
+
+        int idCliente = listaClientes.size() + 1;
+        Cliente cliente = new Cliente(idCliente, dni, nombre, apellidos, email, telefono, fechaNacimiento);
+        listaClientes.add(cliente);
+
+        System.out.println("Cliente dado de alta correctamente");
+        System.out.println(cliente);
+
+    }
+
+    public void listarClientes(){
+        if(listaClientes.isEmpty()){
+            System.out.println("No hay clientes registrados");
+        } else {
+            for(Cliente cliente : listaClientes) {
+                System.out.println(cliente);
+            }
+        }
     }
 }
