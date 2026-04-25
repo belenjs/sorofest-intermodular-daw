02237afc3c90da8f7005d2f2035c99ca3f1ea90f
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 22:46:07 +0200

    feat: se agrega funcionalidad de modificar los datos de un cliente

--- app/SoroFest/src/main/java/controller/ClienteController.java
+++ app/SoroFest/src/main/java/controller/ClienteController.java
@@ -31,7 +31,7 @@ public class ClienteController {
                     case 1 -> darAltaCliente();
                     case 2 -> listarClientes();
                     case 3 -> buscarCliente();
-                    case 4 -> System.out.println("Modificar cliente");
+                    case 4 -> modificarDatosCliente();
                     case 5 -> System.out.println("Eliminar cliente");
                     case 0 -> System.out.println("Volviendo al menú principal...");
                     default -> System.out.println("Opción no válida.");
@@ -87,6 +87,35 @@ public class ClienteController {
             if(cliente.getDni().equalsIgnoreCase(dniCliente)) {
                 System.out.println(cliente);
                 encontrado = true;
+                break;
+            }
+        }
+        if(!encontrado) {
+            System.out.println("No se ha encontrado ningún cliente con dicho dni");
+        }
+    }
+
+    public void modificarDatosCliente(){
+        System.out.println("Introduce el dni del cliente a modificar");
+        String dniCliente = scanner.nextLine();
+        boolean encontrado = false;
+
+        for(Cliente cliente : listaClientes) {
+            if (cliente.getDni().equalsIgnoreCase(dniCliente)) {
+                encontrado = true;
+                System.out.println("Nuevo nombre: ");
+                cliente.setNombre(scanner.nextLine());
+                System.out.println("Nuevos apellidos: ");
+                cliente.setApellidos(scanner.nextLine());
+                System.out.println("Nuevo email: ");
+                cliente.setEmail(scanner.nextLine());
+                System.out.println("Nuevo teléfono: ");
+                cliente.setTelefono(scanner.nextLine());
+                System.out.println("Nueva fecha de nacimiento (yyyy-MM-dd): ");
+                cliente.setFechaNacimiento(LocalDate.parse(scanner.nextLine()));
+
+                System.out.println("Cliente modificado correctamente");
+                System.out.println(cliente);
                 break;
             }
         }
