1ccd56038f0306d1f34f2de3efb2d2638aeadba3
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 18:08:05 +0200

    feat: se añade clase de vista para el menú principal de la app

---
+++ app/SoroFest/src/main/java/view/MenuGestion.java
@@ -0,0 +1,16 @@
+package view;
+
+public class MenuGestion {
+
+    public void mostrarMenuPrincipal(){
+        System.out.println("--- SOROFEST ---");
+        System.out.println("Menú de Gestión");
+        System.out.println("1. Gestionar ediciones");
+        System.out.println("2. Gestionar artistas");
+        System.out.println("3. Gestionar conciertos");
+        System.out.println("4. Gestionar clientes");
+        System.out.println("5. Gestionar entradas");
+        System.out.println("6. Gestionar compras");
+        System.out.println("Por favor, selecciona una opción ");
+    }
+}
