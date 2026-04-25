db808d7cd2ddea23a7dd32b38fdd019894b5964c
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-25 12:49:44 +0200

    feat: se añade clase modelo entrada

---
+++ app/SoroFest/src/main/java/model/Entrada.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Entrada {
+    private int idEntrada;
+    private Edicion edicion;
+    private Compra compra;
+    private String codigoEntrada;
+
+    public Entrada(){
+
+    }
+
+    public Entrada(int idEntrada, Edicion edicion, Compra compra, String codigoEntrada) {
+        this.idEntrada = idEntrada;
+        this.edicion = edicion;
+        this.compra = compra;
+        this.codigoEntrada = codigoEntrada;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrada{" +
+                "idEntrada=" + idEntrada +
+                ", edicion=" + edicion +
+                ", compra=" + compra +
+                ", codigoEntrada='" + codigoEntrada + '\'' +
+                '}';
+    }
+
+    public int getIdEntrada() {
+        return idEntrada;
+    }
+
+    public void setIdEntrada(int idEntrada) {
+        this.idEntrada = idEntrada;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Edicion getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(Edicion edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getCodigoEntrada() {
+        return codigoEntrada;
+    }
+
+    public void setCodigoEntrada(String codigoEntrada) {
+        this.codigoEntrada = codigoEntrada;
+    }
+}
