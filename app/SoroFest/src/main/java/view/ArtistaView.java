e81523f4a6daa0d1e891794ae8c6b49064494a82
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-26 09:47:47 +0200

    feat: se añade clase vista del menú para artistas

---
+++ app/SoroFest/src/main/java/view/ArtistaView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class ArtistaView {
+
+    public void mostrarMenuArtista(){
+        System.out.println("--- GESTIÓN DE ARTISTAS ---");
+        System.out.println("1. Dar de alta artista");
+        System.out.println("2. Listar artistas");
+        System.out.println("3. Buscar artista");
+        System.out.println("4. Modificar artista");
+        System.out.println("5. Eliminar artista");
+        System.out.println("0. Volver");
+        System.out.print("Selecciona una opción: ");
+    }
+}
