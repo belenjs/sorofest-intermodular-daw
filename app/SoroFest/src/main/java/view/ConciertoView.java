3233e0a3aa88372a87e97b71d99a3d961b8c93c8
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-26 11:38:07 +0200

    feat: se añade vista de menú para clase Concierto

---
+++ app/SoroFest/src/main/java/view/ConciertoView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class ConciertoView {
+
+    public void mostrarMenuConcierto(){
+        System.out.println("--- GESTIÓN DE CONCIERTOS ---");
+        System.out.println("1. Dar de alta concierto");
+        System.out.println("2. Listar conciertos");
+        System.out.println("3. Buscar concierto");
+        System.out.println("4. Modificar concierto");
+        System.out.println("5. Eliminar concierto");
+        System.out.println("0. Volver");
+        System.out.print("Selecciona una opción: ");
+    }
+}
