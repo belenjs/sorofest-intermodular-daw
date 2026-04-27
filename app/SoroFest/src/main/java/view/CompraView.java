ce9e2f2a0a42bd06cb4e5da32b6881490a0caf37
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-27 10:12:41 +0200

    feat: se añade clase vista para mostrar el menú de las compras de entradas

---
+++ app/SoroFest/src/main/java/view/CompraView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class CompraView {
+
+    public void mostrarMenuCompra(){
+        System.out.println("--- GESTIÓN DE COMPRAS ---");
+        System.out.println("1. Dar de alta compra de entradas");
+        System.out.println("2. Listar compras de entradas");
+        System.out.println("3. Buscar compra");
+        System.out.println("4. Modificar compra");
+        System.out.println("5. Eliminar compra");
+        System.out.println("0. Volver");
+        System.out.print("Selecciona una opción: ");
+    }
+}
