ef9baee375af5836920b2202bb50268d1424c3fc
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-27 13:14:13 +0200

    feat: clase vista para entrada con su menú principal

---
+++ app/SoroFest/src/main/java/view/EntradaView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class EntradaView {
+
+    public void mostrarMenuEntrada(){
+        System.out.println("--- GESTIÓN DE ENTRADAS ---");
+        System.out.println("1. Dar de alta entrada");
+        System.out.println("2. Listar entradas");
+        System.out.println("3. Buscar entrada");
+        System.out.println("4. Modificar entrada");
+        System.out.println("5. Eliminar entrada");
+        System.out.println("0. Volver");
+        System.out.print("Selecciona una opción: ");
+    }
+}
