d06c53f9937df276530edefc88c574a78f6b9c9c
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-28 10:41:58 +0200

    feat: se añade método para insertar nuevos clientes a la bbdd

--- app/SoroFest/src/main/java/dao/ClienteDAO.java
+++ app/SoroFest/src/main/java/dao/ClienteDAO.java
@@ -1,10 +1,10 @@
 package dao;
 
 import database.ConexionBD;
+import database.SchemaBD;
+import model.Cliente;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class ClienteDAO {
     private Connection connection;
@@ -13,5 +13,28 @@ public class ClienteDAO {
 
     public ClienteDAO(){
         connection = ConexionBD.getConnection();
+    }
+
+    public boolean insertarCliente(Cliente cliente) throws SQLException {
+        String query = String.format(
+                "INSERT INTO %s (%s, %s, %s, %s, %s, %s, %s) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                SchemaBD.COL_ID_CLIENTE,
+                SchemaBD.COL_DNI,
+                SchemaBD.COL_NOMBRE,
+                SchemaBD.COL_APELLIDOS,
+                SchemaBD.COL_EMAIL,
+                SchemaBD.COL_TELEFONO,
+                SchemaBD.COL_FECHA_NACIMIENTO
+        );
+
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, cliente.getDni());
+        preparedStatement.setString(2, cliente.getNombre());
+        preparedStatement.setString(3, cliente.getApellidos());
+        preparedStatement.setString(4, cliente.getEmail());
+        preparedStatement.setString(5, cliente.getTelefono());
+        preparedStatement.setDate(6, Date.valueOf(cliente.getFechaNacimiento()));
+
+        return preparedStatement.execute();
     }
 }
