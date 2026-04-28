41d6739f477ffe057f4bf773448e61319ed50001
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-28 09:18:33 +0200

    feat: se añade clase y método para la conexión a la base de datos con su dependiencia

---
+++ app/SoroFest/src/main/java/database/ConexionBD.java
@@ -0,0 +1,28 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    private static Connection connection;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            createConnection();
+        }
+        return connection;
+    }
+
+    private static void createConnection(){
+        String user = "root";
+        String pass = "";
+        String database = "SoroFest";
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+ database, user, pass);
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión con la base de datos");
+            System.out.println(e.getMessage());
+        }
+    }
+}
