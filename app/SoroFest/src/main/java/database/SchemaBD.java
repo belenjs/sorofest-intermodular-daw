daa42cd49629d8e8c214dc75775489bf3d9eee44
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-28 10:16:33 +0200

    feat: se crea esquema para manejo de variables

---
+++ app/SoroFest/src/main/java/database/SchemaBD.java
@@ -0,0 +1,13 @@
+package database;
+
+public interface SchemaBD {
+    // TABLA CLIENTE
+    String TAB_CLIENTE = "Cliente";
+    String COL_ID_CLIENTE = "id_cliente";
+    String COL_DNI = "dni";
+    String COL_NOMBRE = "nombre";
+    String COL_APELLIDOS = "apellidos";
+    String COL_EMAIL = "email";
+    String COL_TELEFONO = "telefono";
+    String COL_FECHA_NACIMIENTO = "fecha_nacimiento";
+}
